a92cb8403c8d3b4f9399a77ab6a572273fe1b5bc
Stefano Zuccalà <stefano.zuccala@yahoo.com>
2021-10-27 14:44:50 +0200

    fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import { Link } from 'react-router-dom'
   
 export default  function Header({ selected, setSelected, searchTerm, setSearchTerm }) {
     return (
