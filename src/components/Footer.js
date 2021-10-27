ffe80e6daeb3ea358e06e47116b80b7060239060
Stefano Zuccalà <stefano.zuccala@yahoo.com>
2021-10-27 12:58:18 +0200

    commit 37

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,10 @@ export default  function Header({ selected, setSelected, searchTerm, setSearchTe
         <Box sx={{ backgroundColor: "darkgray", height: "auto" }} p={4}>
             <Grid container justifyContent="end">
                 <Grid item>
-                    <Link style={{ color: "blue", textDecoration: "none" }}
-                        onClick={()=>window.location = 'https://theuselessweb.com/'}>TWITTER</Link> | 
-                    <Link style={{ color: "blue", textDecoration: "none" }}
-                        onClick={()=>window.location = 'https://theuselessweb.com/'}>FACEBOOK</Link>
+                    <a style={{ color: "blue", textDecoration: "none" }}
+                        href='https://static.wikia.nocookie.net/5ef5d627-c162-4309-ab47-e09f6b411883'>TWITTER</a> | 
+                    <a style={{ color: "blue", textDecoration: "none" }}
+                        href='https://i.pinimg.com/originals/d8/90/92/d890923215813484bdc0f9544aeca1a9.gif'>FACEBOOK</a>
                 </Grid>
             </Grid>
         </Box>
